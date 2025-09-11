e80adac6b52cae6895790d3d7913619bedde33cf
autoreach <agautoreach@gmail.com>
2025-09-11 01:09:55 -0300

    google3

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -7,21 +7,30 @@ class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>AUTO REACH</title>  {/* Título padrão */}
+          <title>AUTO REACH</title> {/* Título padrão */}
 
           {/* Google Tag Manager */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17549190858"></script>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-17549190858"></script>
           <script>
             {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'AW-17549190858');
+              gtag('config', 'AW-17549190858'); // Substitua pelo seu ID do Google Ads
             `}
           </script>
-          {/* Fim Google Tag Manager */}
+          {/* Fim do Google Tag Manager */}
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-17549190858" // Substitua com o seu ID do GTM
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
