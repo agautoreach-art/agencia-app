5b1900e99f779ce5ead9f302c5c8497098c48a27
autoreach <agautoreach@gmail.com>
2025-09-10 23:30:42 -0300

    sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  siteUrl: 'agenciareach.com.br', // Substitua pelo seu domínio
-  generateRobotsTxt: true, // Opcional: cria o arquivo robots.txt automaticamente
-  sitemapSize: 7000, // Máximo de URLs por sitemap
-}
+  siteUrl: 'https://agenciareach.com.br',  // Coloque seu domínio real aqui
+  generateRobotsTxt: true,  // Isso vai gerar o robots.txt automaticamente
+  sitemapSize: 7000,  // Se o seu site tiver muitas páginas, você pode aumentar isso
+  exclude: ['/404', '/admin'],  // Exclui páginas desnecessárias
+};
