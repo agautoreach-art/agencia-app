9c2ad8eeda7ffda92fdec65f330df5bceb3f5d71
autoreach <agautoreach@gmail.com>
2025-09-14 00:29:18 -0300

    tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Auto Reach</title> {/* Título padrão */}
+       <script
+        dangerouslySetInnerHTML={{__html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TWG4NLZ8')`,
+}}>
+       </script>
+
+      </head>
+
+      
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        
+        <iframe 
+        src="https://www.googletagmanager.com/ns.html?id=GTM-TWG4NLZ8"
+        height="0" 
+        width="0" 
+        
+        ></iframe>
+        
         {children}
       </body>
     </html>
