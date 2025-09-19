e8b0b64b9462e7770d3a0327347ccceeb0e3a651
autoreach <agautoreach@gmail.com>
2025-09-19 20:38:46 -0300

    rastreamento

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,21 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "tdfswfyhbe");
+            `,
+          }}
+        />
+
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-PVNF17B9L6"
